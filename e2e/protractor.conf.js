1056d85bf69167f82a8fe78c2bca7105fb426d1e
Matthias Quintero <matthiasquintero@gmail.com>
2020-03-23 02:45:25 -0600

    fixed base url

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -16,7 +16,7 @@ exports.config = {
     browserName: 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://playlistnrg.s3-website.us-east-2.amazonaws.com/',
+  baseUrl: 'http:/localhost:4200',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
